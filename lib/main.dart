055c0bb6c50e242d38d8017941fca7d63a39408a
Tuan Hung <tuanhung.nguyen.1980@gmail.com>
2023-08-20 14:58:59 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_layout/basic_screen.dart';
+import 'package:flutter_layout/immutable_widget.dart';
+void main() => runApp(const StaticApp());
+
+class StaticApp extends StatelessWidget {
+  const StaticApp({super.key});
+  @override
+  Widget  build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData (
+        primarySwatch: Colors.blue,
+      ),
+      home: const BasicScreen(),
+    );
+  }
+}
